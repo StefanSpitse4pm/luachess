f3220c5a01bcb4f5fc2e477161ed66418f4862b6
StefanSpitse4pm <stefan.spitse@student.nhlstenden.com>
2026-04-06 23:39:01 +0200

    Revert "sub module changes"
    This reverts commit 5451fd27e101fa3eca1d132127c593578e7c7f89.

--- src/Chess/Piece.h
+++ src/Chess/Piece.h
@@ -1,26 +1,3 @@
-/*
-* MIT License
-
-* Copyright (c) [year] [fullname]
-
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-*of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
 //
 // Created by stefanspitse on 2/16/26.
 //
@@ -35,7 +12,7 @@
 
 class Piece
 {
-  public:
+public:
     std::array<int, 2> position; // position[0] = row, position[1] = col
     std::string type;
     std::string image;
@@ -44,7 +21,10 @@ class Piece
     bool canJumpOverPieces = false;
     std::string color;
 
-    Piece(const std::array<int, 2>& position, std::string type, std::string image, std::string color)
+    Piece(
+        const std::array<int, 2>& position, std::string  type, std::string  image,
+        std::string  color
+    )
         : position(position), type(std::move(type)), image(std::move(image)), color(std::move(color))
     {
     }
