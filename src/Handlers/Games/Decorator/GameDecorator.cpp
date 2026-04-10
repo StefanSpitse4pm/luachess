134c7c6301c421b436df30c82df5704d6ef14b26
StefanSpitse4pm <stefan.spitse@student.nhlstenden.com>
2026-04-10 16:32:37 +0200

    Added game decorator

---
+++ src/Handlers/Games/Decorator/GameDecorator.cpp
@@ -0,0 +1,22 @@
+//
+// Created by stefanspitse on 4/10/26.
+//
+
+#include "GameDecorator.h"
+
+#include <nlohmann/json.hpp>
+
+void GameDecorator::start() const
+{
+    this->wrapped.start();
+}
+
+void GameDecorator::stop()
+{
+    this->wrapped.stop();
+}
+
+nlohmann::json GameDecorator::applyMove(const sendMove& move) const
+{
+    return this->wrapped.applyMove();
+}
--- src/Handlers/Games/Decorator/GameDecorator.h
+++ src/Handlers/Games/Decorator/GameDecorator.h
@@ -1,10 +1,28 @@
 //
-// Created by stefanspitse on 4/7/26.
+// Created by stefanspitse on 4/10/26.
 //
 
 #ifndef LUACHESS_GAMEDECORATOR_H
 #define LUACHESS_GAMEDECORATOR_H
+#include "../Game.h"
+#include "../../ActionContext.h"
+#include <nlohmann/json_fwd.hpp>
 
+class GameDecorator : public Game
+{
+  public:
+    explicit GameDecorator(const Game& source) : wrapped(source)
+    {
 
+    }
+    void start() const override;
+    void stop() override;
+    [[nodiscard]] nlohmann::json applyMove(const sendMove& move) const override;
+  protected:
+    Game wrapped;
+
+};
 
 #endif //LUACHESS_GAMEDECORATOR_H
+
+
